a79dfc392f8d29480d6feba4213471a4b1c47d69
ArtemGrom <art.gromov@bk.ru>
2022-03-24 17:05:14 +0300

    create login and logout

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserModel(models.Model):
+    GENDER_ID = [
+        ('M', 'male'),
+        ('F', 'female'),
+    ]
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    gender = models.CharField(max_length=1, choices=GENDER_ID, null=True)
+    image = models.ImageField()
+    email = models.EmailField()
